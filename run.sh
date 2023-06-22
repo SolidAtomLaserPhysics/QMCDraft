ad3ccd68aee7c1604de3de8853419f1342e4edc2
Nico Hyttrek <nhyttrek@th1-2021-d03.PHYSnet.uni-hamburg.de>
2023-06-22 17:29:43 +0200

    module load openmpi

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-
+module load gcc/8.3.0
+module load openmpi/gcc.9/4.1.4
 
 mpif90 hofstadter_hamiltonian.f90 -o hofstadter_hamiltonian.x -g -ffixed-line-length-0
 mpirun --oversubscribe ./hofstadter_hamiltonian.x
