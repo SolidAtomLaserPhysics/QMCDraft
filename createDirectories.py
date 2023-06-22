80b508a4073b38f0692e009c1b1b306fb2148f9e
Nico Hyttrek <nhyttrek@th1-2021-d03.PHYSnet.uni-hamburg.de>
2023-06-22 16:52:50 +0200

    some small bugfixes to achieve that

--- createDirectories.py
+++ createDirectories.py
@@ -28,6 +28,8 @@ def createParameters(u, mu, beta, q, t, tPri, tPriPri, QMCDraftDirectory, QMCCal
             data[31] = "Udd             = {} #-> Hubbard U     \n".format(u)
             data[37] = "Nmeas           = 1e5   \n"
             data[38] = "NCorr           = 20 #-> Wie viele Schritte zwischen zwei Messungen. \n"
+    with open(QMCCalculationDirectory + '/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}/Parameters_Q3.in'.format(u,beta,q,mu,t,tPri,tPriPri), 'w', encoding='utf-8') as file:
+        file.writelines(data)
     
     if q == 4:
         with open(QMCCalculationDirectory + '/Parameters_Q4.in', 'r', encoding='utf-8') as file:
@@ -46,7 +48,7 @@ def createParameters(u, mu, beta, q, t, tPri, tPriPri, QMCDraftDirectory, QMCCal
             data[41] = "Nmeas           = 1e5   \n"
             data[42] = "NCorr           = 20 #-> Wie viele Schritte zwischen zwei Messungen. \n"
     
-    with open(QMCCalculationDirectory + '/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}/Parameters.in'.format(u,beta,q,mu,t,tPri,tPriPri), 'w', encoding='utf-8') as file:
+    with open(QMCCalculationDirectory + '/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}/Parameters_Q4.in'.format(u,beta,q,mu,t,tPri,tPriPri), 'w', encoding='utf-8') as file:
         file.writelines(data)
 
 
