eba9a5a90eb8a1089e5f67d008accb36e9e5b3a5
Nico Hyttrek <nhyttrek@th1-2021-d03.PHYSnet.uni-hamburg.de>
2023-06-23 16:41:52 +0200

    should integrate nCorr now

--- ProcessingQmcHLRN.py
+++ ProcessingQmcHLRN.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
 
 #little testing values
     U = [1.5]
-    Mu = [-2.0, -1.8, -1.6, -1.4, -1.2, -1.0, -0.8, -0.6, -0.4, -0.2, 0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
+    Mu = [4.0]
     Beta = [50.0]
     Q = [3]
     T = [0.25]
@@ -32,10 +32,13 @@ if __name__ == "__main__":
 
     createDirectories = True
     preCalculation = True
-    prepareForCalculation = False
+    prepareForAndDoNextCalculation = False
+    extractResults = False
+    plotResults = False
 
     QMCCalculationDirectory = '/scratch/usr/hhpnhytt/finalQMC/QMCFindMu'
     QMCDraftDirectory = '/scratch/usr/hhpnhytt/finalQMC/QMCDraft'
+    QMCHDF5BackupFolder = '/scratch/usr/hhpnhytt/finalQMC/HDF5BackupFolder'
 
     for u in U:
         for mu in Mu:
@@ -43,15 +46,17 @@ if __name__ == "__main__":
                 for q in Q:
                     for t in T:
                         for tPri in TPrime:
-                            for tPriPri in TPrimePrime:           
+                            for tPriPri in TPrimePrime: 
+                                '''
+                                all directories and files will be generated and put into its folder
+                                after this, everything is ready to start the Calculation
+                                '''          
                                 if createDirectories:
                                     CreateDir.createDirectories(QMCCalculationDirectory + '/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}'.format(u,beta,q,mu,t,tPri,tPriPri))
                                     CreateDir.createParameters(u, mu, beta, q, t, tPri, tPriPri, QMCDraftDirectory, QMCCalculationDirectory)
                                     CreateDir.createHamiltonian(q, QMCDraftDirectory, QMCCalculationDirectory, beta, u, mu,t,tPri,tPriPri)
                                     CreateDir.createSubmit(QMCDraftDirectory, QMCCalculationDirectory, u,beta,q,mu,t,tPri,tPriPri)
                                     CreateDir.createRun(QMCDraftDirectory, QMCCalculationDirectory, u,beta,q,mu,t,tPri,tPriPri)
-                                    #CreateDir.createProduction(QMCDraftDirectory, QMCCalculationDirectory, u,beta,q,mu,t,tPri,tPriPri)
-
 
                                     #makes that run.sh runable
                                     #switches to that directory, since cd does not work with subprocess
@@ -61,22 +66,52 @@ if __name__ == "__main__":
                                     os.system('./run.sh')           #now have epsilonMatrix as .dat file
 
 
+
+
+                                '''
+                                start the first Calculation to get starting point of nCorr
+                                '''
                                 if preCalculation:
                                     os.chdir(QMCCalculationDirectory + '/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}'.format(u,beta,q,mu,t,tPri,tPriPri))
                                     os.system('sbatch submit_dmft_script' )
 
 
-                                if prepareForCalculation:
-                                    #get Ncorr
-                                    hdf5Name = 'U{}_mu{}_B{}_L{}_t{}_tPri{}_tPriPri{}_DMFTPreCalc.hdf5'.format(u,mu,beta,q,t,tPri,tPriPri)
+
+
+                                '''
+                                prepare and do the next calculation
+                                for that you rename the latest hdf5 file to an easy name
+                                older hdf5 will we removed but also backuped in a folder
+                                '''
+                                if prepareForAndDoNextCalculation:
                                     os.chdir(QMCCalculationDirectory + '/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}'.format(u,beta,q,mu,t,tPri,tPriPri))
                                     #rename hdf5 from last calc to an easier name
+                                    #before you remove the hdf5 file from last calculation and name the hdf5 from that calc to the easier name
+                                    #NOTE: never do it twice, only with new calc
+                                    hdf5Name = 'U{}_mu{}_B{}_L{}_t{}_tPri{}_tPriPri{}_DMFT.hdf5'.format(QMCHDF5BackupFolder, u,mu,beta,q,t,tPri,tPriPri)
+                                    try:
+                                        os.system('cp {}/finalQMC_U{}_B_{}_q{}_mu{}_t{}_tPri{}_tPriPri{}'.format(u,beta,q,mu,t,tPri,tPriPri))
+                                        os.system('rm {}'.format(hdf5Name))
+                                    except:
+                                        print("no hdf5 file with wanted name before == preCalculation")
                                     os.system('mv *.hdf5 {}'.format(hdf5Name))
-                                    x = subprocess.check_output(['julia /scratch/projects/hhp00048/codes/scripts/LadderDGA_utils/ncorr.jl {}'.format(hdf5Name)])
+                                    #write nCorr into Parameters.in so that it is ready for real calculation
+                                    nCorr = subprocess.check_output(['julia /scratch/projects/hhp00048/codes/scripts/LadderDGA_utils/ncorr.jl {}'.format(hdf5Name)])
+                                    changeToProd.createSubmit(QMCDraftDirectory, QMCCalculationDirectory, u,beta,q,mu,t,tPri,tPriPri)
+                                    changeToProd.createParameters(u, mu, beta, q, t, tPri, tPriPri, QMCDraftDirectory, QMCCalculationDirectory, nCorr)
                                     
-                                    
-                                    #os.system('julia /scratch/projects/hhp00048/codes/scripts/LadderDGA_utils/ncorr.jl U2.0_mu1.0_beta30.0_DMFT-2023-03-15-Wed-14-41-21.hdf5')
 
+
+
+                                if extractResults:
+                                    #TODO:  still need to implement to extract selfenergy with that julia script
+                                    #       extract density (with julia script)
+                                    pass
+
+                                if plotResults:
+                                    #TODO: plot self energy
+                                    #       plot density
+                                    pass
 
 
 
---
+++ readQMCResults.jl
@@ -0,0 +1,31 @@
+using HDF5
+using DelimitedFiles
+
+
+
+U = ARGS[1]
+Beta = 3ARGS[2]
+q = ARGS[3]
+mu = ARGS[4]
+t = ARGS[5]
+tPrime = ARGS[6]
+tPrimePrime = ARGS[7]
+QMCCalculationDirectory = ARGS[8]
+
+
+fileName = QMCCalculationDirectory + "/finalQMC_U$(U)_B_$(Beta)_q$(q)_mu$(mu)_t$(t)_tPri$(tPrime)_tPriPri$(tPrimePrime)/U$(U)_mu$(mu)_B$(Beta)_L$(q)_t$(t)_tPri$(tPrime)_tPriPri$(tPrimePrime).hdf5"
+
+
+f = h5open(fileName, "r")
+Values = read(f["dmft-last/ineq-001/siw/value"])
+Frequencies = read(f[".axes/iw"])
+
+siw = Values[:,1,1]
+
+RealPart = [real(siw[800:1200])]                                #apparently Real Part is 2D array but only 1 element in that one direction. In reality vector
+ImagPart = [imag(siw[800:1200])]
+MatsubaraFreq = Frequencies[800:1200]
+
+vv = [MatsubaraFreq RealPart[1] ImagPart[1]]
+
+writedlm("self-en_wim_QMC-beta$(Beta)_U$(U)_P$(P)_L$(L).csv", vv)
